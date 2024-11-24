12ea1da3b7fa5d70459d6ce100b727e359c8a26a
cardosofiles <cardosofiles@outlook.com>
2024-11-24 00:00:27 -0300

    configuring docker for postgresql container

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
+import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
 
 export const app = fastify();
 
-app.addHook("preHandler", async (request, reply) => {
+app.addHook("preHandler", async (request) => {
   console.log(`[${request.method}] ${request.url}`);
+});
+
+const prisma = new PrismaClient();
+prisma.user.create({
+  data: {
+    username: "Cardoso Files",
+    email: "cardosofiles@outlook.com",
+  },
 });
