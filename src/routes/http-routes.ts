5e0dbf2e80c763b5b65540959c75a4c16f3d3598
cardosofiles <cardosofiles@outlook.com>
2025-01-26 20:20:27 -0300

    authentication principles

--- src/routes/http-routes.ts
+++ src/routes/http-routes.ts
@@ -1,9 +1,12 @@
 import { FastifyInstance } from "fastify";
 
 import { authenticate } from "@/routes/controller/authenticate";
+import { profile } from "@/routes/controller/profile";
 import { register } from "@/routes/controller/register";
 
 export async function appRoutes(app: FastifyInstance) {
   app.post("/users", register);
   app.post("/sessions", authenticate);
+
+  app.get("/me", profile);
 }
